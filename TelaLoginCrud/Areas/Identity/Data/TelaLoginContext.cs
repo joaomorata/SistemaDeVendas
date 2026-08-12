630a788c0876201d40428673b30e196c59c20372
davidosilva9-hub <david.o.silva9@aluno.senai.br>
2026-08-12 10:47:42 -0300

    Adicionar arquivos de projeto.

---
+++ TelaLoginCrud/Areas/Identity/Data/TelaLoginContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TelaLoginCrud.Areas.Identity.Data;
+using TelaLoginCrud.Models;
+
+namespace TelaLoginCrud.Areas.Identity.Data;
+
+public class TelaLoginContext : IdentityDbContext<Usuario>
+{
+    public TelaLoginContext(DbContextOptions<TelaLoginContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Venda> Venda { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        builder.ApplyConfiguration(new ConfigUser());
+    }
+
+    public class ConfigUser : IEntityTypeConfiguration<Usuario>
+    {
+        public void Configure(EntityTypeBuilder<Usuario> builder)
+        {
+            builder.Property(x => x.Nome).HasMaxLength(150);
+            builder.Property(x => x.Sobrenome).HasMaxLength(150);
+        }
+    }
+}
---
+++ TelaLoginCrud/Models/Venda.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TelaLoginCrud.Models
+{
+    public class Venda
+    {
+        [Key]
+        public int IdVenda { get; set; }
+
+        public string NomeProd { get; set; }
+
+        public int Quant { get; set; }
+
+        public decimal Preco { get; set; }
+
+        public DateTime DataVenda { get; set; }
+    }
+}
